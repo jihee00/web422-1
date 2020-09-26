6b1d65bb271c00a636aa3f452339b720fb38da8b
Jihee <warmgrey32@gmail.com>
2020-09-25 22:28:56 -0400

    workshop1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 * (including web sites) or distributed to other students.
 *
 * Name: _Jihee Kim_____ Student ID: _141389189____ Date: _2020-09-25_____
-* Heroku Link: _________________________________________________
+* Heroku Link: https://web422-1.herokuapp.com/
 *
 ********************************************************************************/ 
 
@@ -28,27 +28,43 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 // POST /api/sales (NOTE: This route must read the contents of the request body)
 
-
+app.post("/api/sales", (req,res)=>{
+    myData.addNewSale(req.body)
+        .then(msg=>res.json(msg))
+        .catch(err=>res.json({message:err}));
+});
 
 // GET /api/sales (NOTE: This route must accept the numeric query parameters "page" and "perPage", ie: /api/sales?page=1&perPage=5 )
 
 app.get("/api/sales", (req,res)=>{
-    myData.getAllSales(1,2).then(data=>{
-        res.json(data);
-    });
+    myData.getAllSales(req.query.page,req.query.perPage)
+        .then(msg=>res.json(msg))
+        .catch(err=>res.json({message:err}));
 });
-
 
 // GET /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
 
-
+app.get("/api/sales/:id", (req,res)=>{
+    myData.getSaleById(req.params.id)
+        .then(sale=>res.json(sale))
+        .catch(err=>res.json({message:err}));
+});
 
 // PUT /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8 as well as read the contents of the request body)
 
-
+app.put("/api/sales/:id", (req,res)=>{
+    myData.updateSaleById(req.body, req.params.id)
+        .then(msg=>res.json(msg))
+        .catch(err=>res.json({message:err}));
+});
 
 // DELETE /api/sales (NOTE: This route must accept a numeric route parameter, ie: /api/sales/5bd761dcae323e45a93ccfe8)
 
+app.delete("/api/sales/:id", (req,res)=>{
+    myData.deleteSaleById(req.params.id)
+        .then(msg=>res.json(msg))
+        .catch(err=>res.json({message:err}));
+});
 
 // ************* Initialize the Service & Start the Server
 
